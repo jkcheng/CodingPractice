9c8c0260bf7992dd3635d605c03fcd79714fdc44
jkcheng <jkcheng@macbookair>
2025-12-08 15:54:12 -0800

    update 199

--- leetcode199.py
+++ leetcode199.py
@@ -65,6 +65,32 @@ def build_tree_recursive(nodes: List[int], i: int, n: int) -> Optional[TreeNode]
         root.right = build_tree_recursive(nodes, 2 * i + 2, n)
     return root
 
+def build_tree_queue(nodes: List[int]) -> Optional[TreeNode]:
+    if len(nodes) == 0:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+    while len(queue) > 0:
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+
+            # add left child
+            if i < len(nodes) and nodes[i] is not None:
+                node.left = TreeNode(nodes[i])
+                queue.append(node.left)
+            i += 1
+
+            # add right child
+            if i < len(nodes) and nodes[i] is not None:
+                node.right = TreeNode(nodes[i])
+                queue.append(node.right)
+            i += 1
+
+    return root
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         # level order traversal
@@ -85,64 +111,70 @@ class Solution:
 
         return ans
 
+from collections import deque # use deque for optimized queue
 class mySolution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # level order traversal
+        # O(nodes), BFS level order traversal
         # add nodes to queue, add last node of each level to result set
-        queue = []
-        ans = []
+        output = []
+        if root is None:
+            return output
 
-        if root is not None:
-            queue.append(root)
+        queue = [root]  # use deque for optimal
 
+        # process nodes level by level
         while len(queue) > 0:
+            # add rightmost node to output
+            output.append(queue[-1].val)
+
+            # process nodes in level
             for _ in range(len(queue)):
                 node = queue.pop(0)
+                # add child nodes to queue
                 if node.left is not None:
                     queue.append(node.left)
                 if node.right is not None:
                     queue.append(node.right)
 
-            ans.append(node.val)
-
-        return ans
+        return output
 
 class testcase1:
     root = [1,2,3,None,5,None,4]
     output = [1,3,4]
-    rootnode = build_tree_recursive(root, 0, len(root))
 
 class testcase2:
     root = [1,2,3,4,None,None,None,5]
     output = [1,3,4,5]
-    rootnode = build_tree_recursive(root, 0, len(root))
 
 class testcase3:
     root = [1,None,3]
     output = [1,3]
-    rootnode = build_tree_recursive(root, 0, len(root))
 
 class testcase4:
     root = []
     output = []
-    rootnode = build_tree_recursive(root, 0, len(root))
+
 
 if __name__ == "__main__":
     # create Solution instance
     soln = mySolution()
 
     # test example 1
-    result1 = soln.rightSideView(testcase1.rootnode)
+    rootnode1 = build_tree_queue(testcase1.root)
+    result1 = soln.rightSideView(rootnode1)
     print(f"Example 1 - Expected: {testcase1.output}, Got: {result1}, Correct: {testcase1.output == result1}" )
 
     # test example 2
-    result2 = soln.rightSideView(testcase2.rootnode)
+    rootnode2 = build_tree_queue(testcase2.root)
+    result2 = soln.rightSideView(rootnode2)
     print(f"Example 2 - Expected: {testcase2.output}, Got: {result2}, Correct: {testcase2.output == result2}" )
 
     # test example 3
-    result3 = soln.rightSideView(testcase3.rootnode)
+    rootnode3 = build_tree_queue(testcase3.root)
+    result3 = soln.rightSideView(rootnode3)
     print(f"Example 3 - Expected: {testcase3.output}, Got: {result3}, Correct: {testcase3.output == result3}" )
 
     # test example 4
-    result4 = soln.rightSideView(testcase4.rootnode)
+    rootnode4 = build_tree_queue(testcase4.root)
+    result4 = soln.rightSideView(rootnode4)
     print(f"Example 4 - Expected: {testcase4.output}, Got: {result4}, Correct: {testcase4.output == result4}" )
